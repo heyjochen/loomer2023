fab0fd76602582ad30fa2faf9305758f8d39dff4
heyjochen <jochen@stierberger.com>
2022-08-16 10:53:12 -0500

    change font

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -10,18 +10,20 @@ const Input = ({ type = '', label = '', className = '', ...props }) => {
   return (
     <div className={classNames(className, 'flex flex-col space-y-1')}>
       {label ? (
-        <label htmlFor="email" className="text-gray-600">
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium text-gray-700"
+        >
           {label}
         </label>
       ) : null}
-
       <div className="flex-1">
         {type === 'textarea' ? (
           <textarea
             {...field}
             {...props}
             className={classNames(
-              'w-full truncate rounded-md border py-2 pl-4 shadow-sm transition focus:outline-none focus:ring-4 focus:ring-opacity-20 disabled:cursor-not-allowed disabled:opacity-50',
+              'block w-full truncate rounded-md border-gray-300 py-2 pl-4 shadow-sm transition focus:border-indigo-500 focus:ring-indigo-500 disabled:cursor-not-allowed disabled:opacity-50 sm:text-sm',
               error
                 ? 'border-red-400 text-red-800 focus:border-red-400 focus:ring-red-400'
                 : 'border-gray-300 focus:border-gray-400 focus:ring-gray-400'
@@ -34,7 +36,7 @@ const Input = ({ type = '', label = '', className = '', ...props }) => {
               {...props}
               type={type}
               className={classNames(
-                'w-full truncate rounded-md border py-2 pl-4 shadow-sm transition focus:outline-none focus:ring-4 focus:ring-opacity-20 disabled:cursor-not-allowed disabled:opacity-50',
+                'block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm',
                 error
                   ? 'border-red-400 text-red-800 focus:border-red-400 focus:ring-red-400'
                   : 'border-gray-300 focus:border-gray-400 focus:ring-gray-400'
@@ -48,7 +50,6 @@ const Input = ({ type = '', label = '', className = '', ...props }) => {
           </div>
         )}
       </div>
-
       {error ? (
         <p name="email" className="text-sm text-red-600 first-letter:uppercase">
           {error}

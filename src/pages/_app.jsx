6ad9f3cd36b550d8a6080c2ffaf937fd97be7949
heyjochen <jochen@stierberger.com>
2022-08-17 09:44:55 -0500

    import Authprovider

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,19 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
+import { Toaster } from 'react-hot-toast'
+import { SessionProvider as AuthProvider } from 'next-auth/react'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <>
+      <AuthProvider session={session}>
+        <Component {...pageProps} />
+      </AuthProvider>
+
+      <Toaster />
+    </>
+  )
 }

a67d466d9eec08cca2cf2fd87299878a8d73dda0
heyjochen <jochen@stierberger.com>
2022-08-17 09:29:44 -0500

    whitelist image domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['vrwdqwonviscyeljayeh.supabase.in'],
+  },
   experimental: {
     newNextLinkBehavior: true,
     images: {

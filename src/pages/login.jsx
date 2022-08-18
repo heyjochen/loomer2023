f890d00f8a2d8ed128b746b001d304700b804991
heyjochen <jochen@stierberger.com>
2022-08-18 14:36:44 -0500

    change login icon

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -18,7 +18,7 @@ const SignInSchema = Yup.object().shape({
     .required('This field is required'),
 })
 
-export default function Login({ show = false, onClose = () => null }) {
+export default function Login() {
   const [disabled, setDisabled] = useState(false)
   const [showConfirm, setConfirm] = useState(false)
   const [showSignIn, setShowSignIn] = useState(false)
@@ -146,11 +146,11 @@ export default function Login({ show = false, onClose = () => null }) {
                   className="h-5 w-5"
                   aria-hidden="true"
                   fill="currentColor"
-                  viewBox="0 0 20 20"
+                  viewBox="0 0 30 30"
                 >
                   <path
                     fillRule="evenodd"
-                    d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z"
+                    d="M 15.003906 3 C 8.3749062 3 3 8.373 3 15 C 3 21.627 8.3749062 27 15.003906 27 C 25.013906 27 27.269078 17.707 26.330078 13 L 25 13 L 22.732422 13 L 15 13 L 15 17 L 22.738281 17 C 21.848702 20.448251 18.725955 23 15 23 C 10.582 23 7 19.418 7 15 C 7 10.582 10.582 7 15 7 C 17.009 7 18.839141 7.74575 20.244141 8.96875 L 23.085938 6.1289062 C 20.951937 4.1849063 18.116906 3 15.003906 3 z"
                     clipRule="evenodd"
                   />
                 </svg>

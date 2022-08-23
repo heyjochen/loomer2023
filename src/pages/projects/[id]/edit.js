8a8b774d3eecf892c997412bd16c8674267e5acd
heyjochen <jochen@stierberger.com>
2022-08-23 12:03:15 -0500

    fix typo

--- src/pages/projects/[id]/edit.js
+++ src/pages/projects/[id]/edit.js
@@ -38,12 +38,12 @@ const Edit = (project = null) => {
     axios.patch(`/api/projects/${project.id}`, data)
 
   return (
-    <div className="mx-auto max-w-screen-sm">
-      <h1 className="text-xl font-medium text-gray-800">Edit your Project</h1>
+    <div className="mx-auto mt-4 max-w-screen-sm px-12">
+      <h1 className="text-xl font-medium text-gray-800">Add your project</h1>
       <p className="text-gray-500">
-        Fill out the form below to update your project.
+        Fill out the form below to list a new project.
       </p>
-      <div className="mt-8">
+      <div className="mt-4">
         {project ? (
           <ListingForm
             initialValues={project}
